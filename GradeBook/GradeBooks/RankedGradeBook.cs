52dd25e1ddb951d2fa53819ead0d261149be9c43
Alec Hauser <alec-h@EMCINS.COM>
2020-04-01 06:36:59 -0500

    Section 3 return letter grade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -11,5 +11,41 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if(Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
+            }
+
+            var threshold = (int)Math.Ceiling(Students.Count * 0.2);
+            List<double> grades = new List<double>();
+            foreach(Student student in Students)
+            {
+                grades.Add(student.AverageGrade);
+            }
+
+            grades.Sort();
+            grades.Reverse();
+
+            if (averageGrade >= grades[threshold-1])
+            {
+                return 'A';
+            }
+            else if (averageGrade >= grades[threshold*2-1])
+            {
+                return 'B';
+            }
+            else if (averageGrade >= grades[threshold*3-1])
+            {
+                return 'C';
+            }
+            else if (averageGrade >= grades[threshold*4-1])
+            {
+                return 'D';
+            }
+
+            return 'F';
+        }
     }
 }
